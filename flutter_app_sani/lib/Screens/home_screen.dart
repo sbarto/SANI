cc462371e2072a9af99779320afb37b42cbe9e62
Gragamella <57292660+Gragamella@users.noreply.github.com>
2022-04-11 16:48:06 +0200

    Inizio creazione HomeScreen

--- flutter_app_sani/lib/Screens/home_screen.dart
+++ flutter_app_sani/lib/Screens/home_screen.dart
@@ -5,18 +5,90 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
-              print("Signed Out");
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LoginPage()));
-            });
-          },
-        ),
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.lightBlue.shade200,
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: size.height * 0.3,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: size.height * 0.25,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(36),
+                      bottomRight: Radius.circular(36),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    height: 54,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 50,
+                            color: Colors.grey.withOpacity(0.30)),
+                      ],
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Cerca",
+                              hintStyle: TextStyle(color: Colors.blue),
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              suffixIcon: Image(
+                                  image: AssetImage("assets/ricerca.png"),
+                                  height: 0.2),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: -140,
+                  right: -120,
+                  width: 250,
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    height: 300,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blueGrey,
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 50,
+                            color: Colors.grey.withOpacity(0.30)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
